5839decdb5794e64c5b9b54df466f3d61ddb42ce
Danny Willems <contact@danny-willems.be>
2016-05-26 13:10:00 +0200

    Add test

--- test/ml/test.ml
+++ test/ml/test.ml
@@ -1,16 +1,34 @@
 module C = Css_materialize
 
-let _ =
-  let container = C.create_container "I'm a container" in
-  let row = C.create_row "I'm a row" in
-  let col1 = C.create_col ~lg:3 "Hello world" in
-  let col2 = C.create_col ~lg:3 "Hello world" in
-  let col3 = C.create_col ~lg:3 "Hello world" in
-  let col4 = C.create_col ~lg:3 "Hello world" in
+let onload _ =
+  C.Head.add_css_materialize C.Head.Bower_css;
+  C.Head.add_icons "fonts/materialize_fonts.css";
 
-  Dom.appendChild row col1;
-  Dom.appendChild row col2;
-  Dom.appendChild row col3;
-  Dom.appendChild row col4;
+  let container = C.create_container "" in
+  let row = C.create_row "" in
+  let col_large = C.create_col ~lg:3 "" in
+  let col_normal = C.create_col ~lg:3 "" in
+  let btn_large =
+    C.create_button ~size:C.Large ~effect:C.Light "Large and Light"
+  in
+  let btn_normal =
+    C.create_button ~size:C.Normal ~effect:C.Waves "Normal and waves"
+  in
+
+  let btn_floating =
+    C.create_button ~size:C.Large ~effect:C.Light ~floating:true ~color:"red" ""
+  in
+  let icon_add = C.Icon.create_icon "add" in
+  Dom.appendChild btn_floating icon_add;
+
+  Dom.appendChild col_large btn_large;
+  Dom.appendChild col_normal btn_normal;
+  Dom.appendChild row col_large;
+  Dom.appendChild row col_normal;
   Dom.appendChild container row;
-  Dom.appendChild Dom_html.document##.body container
+  Dom.appendChild container btn_floating;
+  Dom.appendChild Dom_html.document##.body container;
+  Js._false
+
+let _  = Dom_html.window##.onload := Dom.handler onload
+
